86f8a49d3fdbef5699b8bdb1bd5a8975e881f47e
r2d2dlg <afdelaguardia@gmail.com>
2025-05-24 17:34:34 -0500

    Remove android-app from version control

--- android-app/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_marta_MartaApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.marta.MartaApplication"
-)
-public final class _com_example_marta_MartaApplication {
-}
